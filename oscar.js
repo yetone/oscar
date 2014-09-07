9f9148d71558b7628adbfc56e3dc04ce08503d6e
yetone <i@yetone.net>
2014-09-08 00:55:29 +0800

    refactor

--- oscar.js
+++ oscar.js
@@ -1,4 +1,15 @@
 ;(function(window, undefined) {
+  var arrProto = window.Array.prototype,
+      def = window.Object.defineProperty,
+      defs = window.Object.defineProperties,
+      getObjKeys = window.Object.keys,
+      isArray = window.Array.isArray;
+  function toArray(obj) {
+    return arrProto.slice.call(obj);
+  }
+  function runWithScope(code, scope) {
+    return (new Function('with(this) {return (' + code + ');}')).call(scope);
+  }
   window.Oscar = (function() {
     function Oscar() {
       this.modelList = [];
@@ -9,110 +20,66 @@
     };
     proto.buildArray = function(arr) {
       var self = this,
-          propertyList = [],
-          properties,
           args;
-      function build(arr) {
-        var propertyList = [];
-        for (var i in arr.__c__) {
-          if (!arr.hasOwnProperty(i)) continue;
-          if (typeof arr[i] === 'function') continue;
-          self.genPropertyList(propertyList, i);
+      ['push', 'pop', 'shift', 'unshift', 'splice'].forEach(function(method) {
+        arr[method] = function() {
+          args = toArray(arguments);
+          _args = toArray(arguments);
+          if (method === 'splice') {
+            var subArgs = args.slice(2);
+            self.buildObj([subArgs]);
+            args = args.slice(0, 2);
+            arrProto.push.apply(args, subArgs);
+          } else {
+            self.buildObj(args);
+          }
+          arrProto[method].apply(this, args);
+          arrProto[method].apply(this.__c__, _args);
+          self.buildObj(this);
+          self.watcher();
+        };
+      });
+      self.buildObj(arr);
+    };
+    proto.buildObj = function(obj) {
+      var self = this,
+          properties = {};
+      if (obj.__c__ === undefined) {
+        if (isArray(obj)) {
+          obj.__c__ = [];
+        } else {
+          obj.__c__ = {};
         }
-        properties = '({' + propertyList.join(', ') + '})';
-        Object.defineProperties(arr, eval(properties));
       }
-      function buildArgs(args) {
-        var a;
-        for (var i = 0, l = args.length; i < l; i++) {
-          a = args[i];
-          switch (a.constructor) {
-            case (window.Array):
-              self.buildArray(a);
-              break;
-            case (window.Object):
-              self.buildData(a);
-              break;
+      getObjKeys(obj).forEach(function(k) {
+        if (k === '__c__') return;
+        if (typeof obj[k] === 'function') return;
+        obj.__c__[k] = obj[k];
+        if (obj[k].constructor === window.Array) {
+          self.buildArray(obj[k]);
+        }
+        if (obj[k].constructor === window.Object) {
+          self.buildObj(obj[k]);
+        }
+        properties[k] = {
+          get: function() {
+            return this.__c__[k];
+          },
+          set: function(v) {
+            switch(v.constructor) {
+              case window.Array:
+                self.buildArray(v);
+                break;
+              case window.Object:
+                self.buildObj(v);
+                break;
+            }
+            this.__c__[k] = v;
+            self.watcher();
           }
         }
-      }
-      arr.__c__ = [];
-      arr.push = function() {
-        args = window.Array.prototype.slice.call(arguments);
-        buildArgs(args);
-        window.Array.prototype.push.apply(this, args);
-        window.Array.prototype.push.apply(this.__c__, args);
-        build(this);
-        self.watcher();
-      };
-      arr.pop = function() {
-        args = window.Array.prototype.slice.call(arguments);
-        window.Array.prototype.pop.apply(this, args);
-        window.Array.prototype.pop.apply(this.__c__, args);
-        build(this);
-        self.watcher();
-      };
-      arr.shift = function() {
-        args = window.Array.prototype.slice.call(arguments);
-        window.Array.prototype.shift.apply(this, args);
-        window.Array.prototype.shift.apply(this.__c__, args);
-        build(this);
-        self.watcher();
-      };
-      arr.unshift = function() {
-        args = window.Array.prototype.slice.call(arguments);
-        buildArgs(args);
-        window.Array.prototype.unshift.apply(this, args);
-        window.Array.prototype.unshift.apply(this.__c__, args);
-        build(this);
-        self.watcher();
-      };
-      arr.splice = function() {
-        args = window.Array.prototype.slice.call(arguments);
-        var subArgs = args.slice(2);
-        buildArgs([subArgs]);
-        args = args.slice(0, 2);
-        window.Array.prototype.push.apply(args, subArgs);
-        window.Array.prototype.splice.apply(this, args);
-        window.Array.prototype.splice.apply(this.__c__, args);
-        build(this);
-        self.watcher();
-      };
-      arr.__build = function() {
-        build(this);
-      };
-      for (var i in arr) {
-        if (!arr.hasOwnProperty(i)) continue;
-        if (typeof arr[i] === 'function') continue;
-        if (i === '__c__') continue;
-        arr.__c__[i] = arr[i];
-        self.genPropertyList(propertyList, i);
-      }
-      properties = '({' + propertyList.join(', ') + '})';
-      Object.defineProperties(arr, eval(properties));
-    };
-    proto.genPropertyList = function(lst, k) {
-      lst.push(k + ': {get: function() {return this.__c__[\'' + k + '\'];}, set: function(v) {if (v.constructor === window.Array) {self.buildArray(v)} if (v.constructor === window.Object) {self.buildData(v)} this.__c__[\'' + k + '\'] = v; self.watcher();}}');
-    };
-    proto.buildData = function(data) {
-      var self = this,
-          propertyList = [],
-          properties;
-      data.__c__ = {};
-      for (var k in data) {
-        if (!data.hasOwnProperty(k)) continue;
-        if (k === '__c__') continue;
-        data.__c__[k] = data[k];
-        if (data[k].constructor === window.Array) {
-          self.buildArray(data[k]);
-        }
-        if (data[k].constructor === window.Object) {
-          self.buildData(data[k]);
-        }
-        self.genPropertyList(propertyList, k);
-      }
-      properties = '({' + propertyList.join(', ') + '})';
-      Object.defineProperties(data, eval(properties));
+      });
+      defs(obj, properties);
     };
     proto.modelRegister = function(obj) {
       if (typeof obj !== 'object' || typeof obj.el !== 'string' ||  typeof obj.data !== 'object') {
@@ -122,7 +89,7 @@
       if (!$els.length) {
         throw new Error('cannot find the element');
       }
-      this.buildData(obj.data);
+      this.buildObj(obj.data);
       var $el = $els[0],
           $binds = $el.querySelectorAll('[oscar-bind]');
       for (var i = 0, l = $binds.length; i < l; i++) {
@@ -190,35 +157,31 @@
           needBind = proto.utils.differ(e.$el, $tmp);
         }
         // oscar-class
-        $classes = e.$el.querySelectorAll('[oscar-class]');
-        for (i = 0, l = $classes.length; i < l; i++) {
-          var cc, ccl;
-          $c = $classes[i];
-          cc = $c.getAttribute('oscar-class');
+        $classes = toArray(e.$el.querySelectorAll('[oscar-class]'));
+        $classes.forEach(function($c, i) {
+          var cc = $c.getAttribute('oscar-class'),
+              ccl;
           try {
             cc = cc.replace(new window.RegExp("'", 'g'), '"');
-            ccl = (new Function('with(this){return (' + cc + ');}')).call(e.data);
+            ccl = runWithScope(cc, e.data);
           } catch(err) {
             console.log(err);
-            break;
+            return;
           }
-          e.data.__$c__ = $c;
-          for (var cls in ccl) {
-            if (!ccl.hasOwnProperty(cls)) continue;
-            if (ccl[cls]) {
-              (new Function('with(this){(__$c__.classList.add(\'' + cls + '\'))}')).call(e.data);
+          getObjKeys(ccl).forEach(function(cls) {
+            if (ccl[cls] === true) {
+              $c.classList.add(cls);
             } else {
-              (new Function('with(this){(__$c__.classList.remove(\'' + cls + '\'))}')).call(e.data);
+              $c.classList.remove(cls);
             }
-          }
-        }
+          });
+        });
         // oscar-class end
         if (e.inited && !needBind) return;
         // oscar-bind
-        $binds = e.$el.querySelectorAll('[oscar-bind]');
-        for (var i = 0, l = $binds.length; i < l; i++) {
-          var $b = $binds[i],
-              bc = $b.getAttribute('oscar-bind'),
+        $binds = toArray(e.$el.querySelectorAll('[oscar-bind]'));
+        $binds.forEach(function($b, i) {
+          var bc = $b.getAttribute('oscar-bind'),
               c = '',
               bcl;
           bc = bc.replace(/(\[|\])/g, '.');
@@ -230,32 +193,29 @@
             if (!bcl.hasOwnProperty(x)) continue;
             c += '[\'' + bcl[x] + '\']';
           }
-          var s = '(e.data' + c + '=this.value)';
+          var s = '(e.data' + c + ' = this.value)';
           var eventType = 'input';
           if ($b.type === 'radio') {
             eventType = 'change';
           }
-          (new Function('with(this){($b.addEventListener(\'' + eventType + '\', function() {' + s + '}))}')).call({
-            e: e,
-            $b: $b
+          $b.addEventListener(eventType, function() {
+            eval(s);
           });
-          (new Function('with(this){if (e.data' + c + ' === $b.value) {$b.hasOwnProperty(\'checked\') && ($b.checked = true)}}')).call({
-            e: e,
-            $b: $b
-          });
-        }
+          if (eval('(e.data' + c + ' === $b.value)')) {
+            $b.hasOwnProperty('checked') && ($b.checked = true);
+          }
+        });
         // oscar-bind end
         // oscar-action
-        $actions = e.$el.querySelectorAll('[oscar-action]');
-        for (i = 0, l = $actions.length; i < l; i++) {
-          var ac, acl, $a;
-          $a = $actions[i];
-          ac = $a.getAttribute('oscar-action');
-          acl = /(\w+):(.*)/g.exec(ac);
-          if (acl.length !== 3) continue;
-          e.data.__$a__ = $a;
-          (new Function('with(this){(__$a__.addEventListener(\'' + acl[1] + '\', function() {' + acl[2] + '}))}')).call(e.data);
-        }
+        $actions = toArray(e.$el.querySelectorAll('[oscar-action]'));
+        $actions.forEach(function($a, i) {
+          var ac = $a.getAttribute('oscar-action'),
+              acl = /(\w+):(.*)/g.exec(ac);
+          if (acl.length !== 3) return;
+          $a.addEventListener(acl[1], function() {
+            runWithScope(acl[2], e.data);
+          });
+        });
         // oscar-action end
         e.inited = true;
       });
