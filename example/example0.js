9ec0650bd0514b5184c2c54f0b16c65c8ff57aa1
yetone <i@yetone.net>
2014-09-10 08:33:14 +0800

    add ex

---
+++ example/example0.js
@@ -0,0 +1,60 @@
+// example
+window.onload = function() {
+  window.data0 = {
+    name: 'yetone',
+    age: 23,
+    skills: [
+      'Python',
+      'Golang',
+      'JavaScript'
+    ],
+    say: function() {
+      return 'It works!';
+    },
+    dict: {
+      a: 1,
+      b: 2,
+      3: 'c'
+    }
+  };
+  window.data1 = {
+    name: 'enotey',
+    age: 12,
+    skills: [
+      'Erlang',
+      'Elixir',
+      'Haskell'
+    ]
+  };
+  window.data2 = {
+    mode: 'a'
+  };
+  window.data3 = {
+    country: 'China'
+  };
+  window.data4 = {
+    countries: ['China', 'Japan'],
+    ok: true
+  };
+  var oscar = new Oscar();
+  window.md = oscar.modelRegister({
+    el: '#model0',
+    data: data0
+  });
+  oscar.modelRegister({
+    el: '#model1',
+    data: data1
+  });
+  oscar.modelRegister({
+    el: '#model2',
+    data: data2
+  });
+  oscar.modelRegister({
+    el: '#model3',
+    data: data3
+  });
+  oscar.modelRegister({
+    el: '#model4',
+    data: data4
+  });
+};
